d17ecf5921e640a8ce2fd36e9045e5b2550831b5
Alexander Emelin <frvzmb@gmail.com>
2018-08-03 01:44:41 +0300

    stick with disconnect in case of expired sub

--- client.go
+++ client.go
@@ -203,7 +203,14 @@ func (c *Client) updatePresence() {
 				// Give subscription a chance to be refreshed via SubRefreshHandler.
 				reply := c.eventHub.subRefreshHandler(SubRefreshEvent{Channel: channel})
 				if reply.Expired || (reply.ExpireAt > 0 && reply.ExpireAt < now) {
-					go c.Unsubscribe(channel, true)
+					// Ideally we should deal with single expired subscription in this
+					// case - i.e. unsubscribe client from channel and give an advice
+					// to resubscribe. But there is scenario when browser goes online
+					// after computer was in sleeping mode which I have not managed to
+					// handle reliably on client side when unsubscribe with resubscribe
+					// flag was used. So I decided to stick with disconnect for now -
+					// it seems to work fine and drastically simplifies client code.
+					go c.Close(DisconnectSubExpired)
 					// No need to update channel presence.
 					continue
 				}
@@ -217,7 +224,7 @@ func (c *Client) updatePresence() {
 				// The only way subscription could be refreshed in this case is via
 				// SUB_REFRESH command sent from client but looks like that command
 				// with new refreshed token have not been received in configured window.
-				go c.Unsubscribe(channel, true)
+				go c.Close(DisconnectSubExpired)
 				// No need to update channel presence.
 				continue
 			}
