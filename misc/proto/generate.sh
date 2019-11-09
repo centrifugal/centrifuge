28c241aa62e05c3453c70cd21c1fee49ea9c6670
Alexander Emelin <frvzmb@gmail.com>
2019-11-09 15:26:25 +0300

    update gogo/protobuf dep

--- misc/proto/generate.sh
+++ misc/proto/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-go get github.com/hairyhenderson/gomplate
+GO111MODULE=off go get github.com/hairyhenderson/gomplate
 
 gomplate -f $GOPATH/src/github.com/centrifugal/centrifuge/misc/proto/client.template.proto > $GOPATH/src/github.com/centrifugal/centrifuge/misc/proto/client.proto
 GOGO=1 gomplate -f $GOPATH/src/github.com/centrifugal/centrifuge/misc/proto/client.template.proto > $GOPATH/src/github.com/centrifugal/centrifuge/internal/proto/client.proto
