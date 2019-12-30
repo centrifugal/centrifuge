6185534aa3f81d06fa60dd4b7eb96d3dd8a57a30
Alexander Emelin <frvzmb@gmail.com>
2019-12-30 12:17:05 +0300

    validate recovery configuration, config validation tests

---
+++ config_test.go
@@ -0,0 +1,61 @@
+package centrifuge
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigValidateDefault(t *testing.T) {
+	err := DefaultConfig.Validate()
+	require.NoError(t, err)
+}
+
+func TestConfigValidateInvalidNamespaceName(t *testing.T) {
+	c := DefaultConfig
+	c.Namespaces = []ChannelNamespace{
+		ChannelNamespace{
+			Name:           "invalid name",
+			ChannelOptions: ChannelOptions{},
+		},
+	}
+	err := c.Validate()
+	require.Error(t, err)
+}
+
+func TestConfigValidateDuplicateNamespaceName(t *testing.T) {
+	c := DefaultConfig
+	c.Namespaces = []ChannelNamespace{
+		ChannelNamespace{
+			Name:           "name",
+			ChannelOptions: ChannelOptions{},
+		},
+		ChannelNamespace{
+			Name:           "name",
+			ChannelOptions: ChannelOptions{},
+		},
+	}
+	err := c.Validate()
+	require.Error(t, err)
+}
+
+func TestConfigValidateMalformedReciverTopLevel(t *testing.T) {
+	c := DefaultConfig
+	c.HistoryRecover = true
+	err := c.Validate()
+	require.Error(t, err)
+}
+
+func TestConfigValidateMalformedReciverInNamespace(t *testing.T) {
+	c := DefaultConfig
+	c.Namespaces = []ChannelNamespace{
+		ChannelNamespace{
+			Name: "name",
+			ChannelOptions: ChannelOptions{
+				HistoryRecover: true,
+			},
+		},
+	}
+	err := c.Validate()
+	require.Error(t, err)
+}
