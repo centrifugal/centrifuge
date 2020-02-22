39bf6dba19983900e1018508f226c6c84bbb9ca9
Alexander Emelin <frvzmb@gmail.com>
2020-02-22 13:14:49 +0300

    add custom_ws_nhooyr example

---
+++ _examples/custom_ws_nhooyr/main.go
@@ -0,0 +1,338 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	_ "net/http/pprof"
+
+	"nhooyr.io/websocket"
+
+	"github.com/centrifugal/centrifuge"
+)
+
+type clientMessage struct {
+	Timestamp int64  `json:"timestamp"`
+	Input     string `json:"input"`
+}
+
+func handleLog(e centrifuge.LogEntry) {
+	log.Printf("%s: %v", e.Message, e.Fields)
+}
+
+func authMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		newCtx := centrifuge.SetCredentials(ctx, &centrifuge.Credentials{
+			UserID:   "42",
+			ExpireAt: time.Now().Unix() + 60,
+			Info:     []byte(`{"name": "Alexander"}`),
+		})
+		r = r.WithContext(newCtx)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func waitExitSignal(n *centrifuge.Node) {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		n.Shutdown(context.Background())
+		done <- true
+	}()
+	<-done
+}
+
+func main() {
+	cfg := centrifuge.DefaultConfig
+
+	// Set HMAC secret to handle requests with JWT auth too. This is
+	// not required if you don't use token authentication and
+	// private subscriptions verified by token.
+	cfg.TokenHMACSecretKey = "secret"
+	cfg.LogLevel = centrifuge.LogLevelInfo
+	cfg.LogHandler = handleLog
+
+	cfg.JoinLeave = true
+	cfg.HistoryLifetime = 300
+	cfg.HistorySize = 1000
+	cfg.HistoryRecover = true
+
+	cfg.UserSubscribePersonal = true
+
+	cfg.Namespaces = []centrifuge.ChannelNamespace{
+		centrifuge.ChannelNamespace{
+			Name: "chat",
+			ChannelOptions: centrifuge.ChannelOptions{
+				Publish:         true,
+				Presence:        true,
+				JoinLeave:       true,
+				HistoryLifetime: 60,
+				HistorySize:     1000,
+				HistoryRecover:  true,
+			},
+		},
+	}
+
+	node, _ := centrifuge.New(cfg)
+
+	node.On().ClientConnecting(func(ctx context.Context, t centrifuge.TransportInfo, e centrifuge.ConnectEvent) centrifuge.ConnectReply {
+		// Subscribe to several server-side channels.
+		subs := []centrifuge.Subscription{}
+		for i := 0; i < 3; i++ {
+			subs = append(subs, centrifuge.Subscription{Channel: "server-side-" + strconv.Itoa(i)})
+		}
+		return centrifuge.ConnectReply{
+			Data:          centrifuge.Raw(`{}`),
+			Subscriptions: subs,
+		}
+	})
+
+	node.On().ClientRefresh(func(ctx context.Context, client *centrifuge.Client, e centrifuge.RefreshEvent) centrifuge.RefreshReply {
+		log.Printf("user %s connection is going to expire, refreshing", client.UserID())
+		return centrifuge.RefreshReply{
+			ExpireAt: time.Now().Unix() + 60,
+		}
+	})
+
+	node.On().ClientConnected(func(ctx context.Context, client *centrifuge.Client) {
+
+		client.On().Subscribe(func(e centrifuge.SubscribeEvent) centrifuge.SubscribeReply {
+			log.Printf("user %s subscribes on %s", client.UserID(), e.Channel)
+			return centrifuge.SubscribeReply{
+				ExpireAt: time.Now().Unix() + 60,
+			}
+		})
+
+		client.On().SubRefresh(func(e centrifuge.SubRefreshEvent) centrifuge.SubRefreshReply {
+			log.Printf("user %s subscription on channel %s is going to expire, refreshing", client.UserID(), e.Channel)
+			return centrifuge.SubRefreshReply{
+				ExpireAt: time.Now().Unix() + 60,
+			}
+		})
+
+		client.On().Unsubscribe(func(e centrifuge.UnsubscribeEvent) centrifuge.UnsubscribeReply {
+			log.Printf("user %s unsubscribed from %s", client.UserID(), e.Channel)
+			return centrifuge.UnsubscribeReply{}
+		})
+
+		client.On().Publish(func(e centrifuge.PublishEvent) centrifuge.PublishReply {
+			log.Printf("user %s publishes into channel %s: %s", client.UserID(), e.Channel, string(e.Data))
+			var msg clientMessage
+			err := json.Unmarshal(e.Data, &msg)
+			if err != nil {
+				return centrifuge.PublishReply{
+					Error: centrifuge.ErrorBadRequest,
+				}
+			}
+			msg.Timestamp = time.Now().Unix()
+			data, _ := json.Marshal(msg)
+			return centrifuge.PublishReply{
+				Data: data,
+			}
+		})
+
+		client.On().RPC(func(e centrifuge.RPCEvent) centrifuge.RPCReply {
+			log.Printf("RPC from user: %s, data: %s", client.UserID(), string(e.Data))
+			return centrifuge.RPCReply{
+				Data: []byte(`{"year": "2018"}`),
+			}
+		})
+
+		client.On().Message(func(e centrifuge.MessageEvent) centrifuge.MessageReply {
+			log.Printf("Message from user: %s, data: %s", client.UserID(), string(e.Data))
+			return centrifuge.MessageReply{}
+		})
+
+		client.On().Disconnect(func(e centrifuge.DisconnectEvent) centrifuge.DisconnectReply {
+			log.Printf("user %s disconnected, disconnect: %#v", client.UserID(), e.Disconnect)
+			return centrifuge.DisconnectReply{}
+		})
+
+		transport := client.Transport()
+		log.Printf("user %s connected via %s with protocol: %s", client.UserID(), transport.Name(), transport.Protocol())
+
+		// Connect handler should not block, so start separate goroutine to
+		// periodically send messages to client.
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(5 * time.Second):
+					err := client.Send(centrifuge.Raw(`{"time": "` + strconv.FormatInt(time.Now().Unix(), 10) + `"}`))
+					if err != nil {
+						if err == io.EOF {
+							return
+						}
+						log.Println(err.Error())
+					}
+				}
+			}
+		}()
+	})
+
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		i := 0
+		for {
+			err := node.Publish(node.PersonalChannel("42"), centrifuge.Raw(`{"message": "personal `+strconv.Itoa(i)+`"}`))
+			if err != nil {
+				log.Println(err.Error())
+			}
+			time.Sleep(5000 * time.Millisecond)
+			i++
+		}
+	}()
+
+	websocketHandler := newWebsocketHandler(node)
+	http.Handle("/connection/websocket", authMiddleware(websocketHandler))
+	http.Handle("/", http.FileServer(http.Dir("./")))
+
+	go func() {
+		if err := http.ListenAndServe(":8000", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	waitExitSignal(node)
+	log.Println("bye!")
+}
+
+type customWebsocketHandler struct {
+	node *centrifuge.Node
+}
+
+func newWebsocketHandler(node *centrifuge.Node) http.Handler {
+	return &customWebsocketHandler{node}
+}
+
+const websocketTransportName = "websocket"
+
+type customWebsocketTransport struct {
+	mu      sync.RWMutex
+	closed  bool
+	closeCh chan struct{}
+
+	conn      *websocket.Conn
+	protoType centrifuge.ProtocolType
+	request   *http.Request
+}
+
+func newWebsocketTransport(conn *websocket.Conn, protoType centrifuge.ProtocolType) *customWebsocketTransport {
+	return &customWebsocketTransport{
+		conn:      conn,
+		protoType: protoType,
+		closeCh:   make(chan struct{}),
+	}
+}
+
+func (t *customWebsocketTransport) Name() string {
+	return websocketTransportName
+}
+
+func (t *customWebsocketTransport) Protocol() centrifuge.ProtocolType {
+	return t.protoType
+}
+
+func (t *customWebsocketTransport) Encoding() centrifuge.EncodingType {
+	return centrifuge.EncodingTypeJSON
+}
+
+func (t *customWebsocketTransport) Write(data []byte) error {
+	select {
+	case <-t.closeCh:
+		return nil
+	default:
+		var messageType = websocket.MessageText
+		if t.Protocol() == centrifuge.ProtocolTypeProtobuf {
+			messageType = websocket.MessageBinary
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		err := t.conn.Write(ctx, messageType, data)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func (t *customWebsocketTransport) Close(disconnect *centrifuge.Disconnect) error {
+	t.mu.Lock()
+	if t.closed {
+		t.mu.Unlock()
+		return nil
+	}
+	t.closed = true
+	close(t.closeCh)
+	t.mu.Unlock()
+
+	if disconnect != nil {
+		reason, err := json.Marshal(disconnect)
+		if err != nil {
+			return err
+		}
+		return t.conn.Close(websocket.StatusCode(disconnect.Code), string(reason))
+	}
+	return t.conn.Close(websocket.StatusNormalClosure, "")
+}
+
+func (s *customWebsocketHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
+	conn, err := websocket.Accept(rw, r, &websocket.AcceptOptions{})
+	if err != nil {
+		s.node.Log(centrifuge.NewLogEntry(centrifuge.LogLevelDebug, "websocket upgrade error", map[string]interface{}{"error": err.Error()}))
+		return
+	}
+
+	var protoType = centrifuge.ProtocolTypeJSON
+	if r.URL.Query().Get("format") == "protobuf" {
+		protoType = centrifuge.ProtocolTypeProtobuf
+	}
+
+	transport := newWebsocketTransport(conn, protoType)
+
+	select {
+	case <-s.node.NotifyShutdown():
+		transport.Close(centrifuge.DisconnectShutdown)
+		return
+	default:
+	}
+
+	c, err := centrifuge.NewClient(r.Context(), s.node, transport)
+	if err != nil {
+		s.node.Log(centrifuge.NewLogEntry(centrifuge.LogLevelError, "error creating client", map[string]interface{}{"transport": websocketTransportName}))
+		return
+	}
+	s.node.Log(centrifuge.NewLogEntry(centrifuge.LogLevelDebug, "client connection established", map[string]interface{}{"client": c.ID(), "transport": websocketTransportName}))
+	defer func(started time.Time) {
+		s.node.Log(centrifuge.NewLogEntry(centrifuge.LogLevelDebug, "client connection completed", map[string]interface{}{"client": c.ID(), "transport": websocketTransportName, "duration": time.Since(started)}))
+	}(time.Now())
+	defer c.Close(nil)
+
+	for {
+		_, data, err := conn.Read(context.Background())
+		if err != nil {
+			return
+		}
+		ok := c.Handle(data)
+		if !ok {
+			return
+		}
+	}
+}
--- _examples/go.mod
+++ _examples/go.mod
@@ -12,10 +12,12 @@ require (
 	github.com/gobwas/ws v1.0.2
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/sessions v1.2.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mailru/easygo v0.0.0-20190618140210-3c14a0dc985f
 	github.com/nats-io/nats-server/v2 v2.1.4 // indirect
 	github.com/nats-io/nats.go v1.9.1
 	github.com/prometheus/client_golang v0.9.2
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	nhooyr.io/websocket v1.7.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	nhooyr.io/websocket v1.8.3
 )
