a461148d78cbb17eb5ab4bc20f166151b1bfbf8c
Alexander Emelin <frvzmb@gmail.com>
2020-12-25 22:16:19 +0300

    Tarantool engine example (#176)

---
+++ _examples/custom_engine_tarantool/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	_ "net/http/pprof"
+
+	"github.com/centrifugal/centrifuge"
+	"github.com/centrifugal/centrifuge/_examples/custom_engine_tarantool/tntengine"
+)
+
+var (
+	port    = flag.Int("port", 8000, "Port to bind app to")
+	sharded = flag.Bool("sharded", false, "Start sharded example")
+	ha      = flag.Bool("ha", false, "Start ha example")
+)
+
+func handleLog(e centrifuge.LogEntry) {
+	log.Printf("[centrifuge] %s: %v", e.Message, e.Fields)
+}
+
+func authMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ctx = centrifuge.SetCredentials(ctx, &centrifuge.Credentials{
+			UserID: "42",
+			Info:   []byte(`{"name": "Alexander"}`),
+		})
+		r = r.WithContext(ctx)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func waitExitSignal(n *centrifuge.Node) {
+	sigCh := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		_ = n.Shutdown(context.Background())
+		done <- true
+	}()
+	<-done
+}
+
+func main() {
+	flag.Parse()
+
+	cfg := centrifuge.DefaultConfig
+	cfg.LogLevel = centrifuge.LogLevelDebug
+	cfg.LogHandler = handleLog
+
+	node, _ := centrifuge.New(cfg)
+
+	node.OnConnect(func(client *centrifuge.Client) {
+		transport := client.Transport()
+		log.Printf("user %s connected via %s with protocol: %s", client.UserID(), transport.Name(), transport.Protocol())
+
+		client.OnSubscribe(func(e centrifuge.SubscribeEvent, cb centrifuge.SubscribeCallback) {
+			log.Printf("user %s subscribes on %s", client.UserID(), e.Channel)
+			cb(centrifuge.SubscribeReply{
+				Options: centrifuge.SubscribeOptions{
+					Presence:  true,
+					JoinLeave: true,
+					Recover:   true,
+				},
+			}, nil)
+		})
+
+		client.OnUnsubscribe(func(e centrifuge.UnsubscribeEvent) {
+			log.Printf("user %s unsubscribed from %s", client.UserID(), e.Channel)
+		})
+
+		client.OnPublish(func(e centrifuge.PublishEvent, cb centrifuge.PublishCallback) {
+			log.Printf("user %s publishes into channel %s: %s", client.UserID(), e.Channel, string(e.Data))
+			cb(centrifuge.PublishReply{
+				Options: centrifuge.PublishOptions{
+					HistorySize: 10,
+					HistoryTTL:  10 * time.Minute,
+				},
+			}, nil)
+		})
+
+		client.OnPresence(func(e centrifuge.PresenceEvent, cb centrifuge.PresenceCallback) {
+			log.Printf("user %s calls presence on %s", client.UserID(), e.Channel)
+			if !client.IsSubscribed(e.Channel) {
+				cb(centrifuge.PresenceReply{}, centrifuge.ErrorPermissionDenied)
+				return
+			}
+			cb(centrifuge.PresenceReply{}, nil)
+		})
+
+		client.OnPresenceStats(func(e centrifuge.PresenceStatsEvent, cb centrifuge.PresenceStatsCallback) {
+			log.Printf("user %s calls presence stats on %s", client.UserID(), e.Channel)
+			if !client.IsSubscribed(e.Channel) {
+				cb(centrifuge.PresenceStatsReply{}, centrifuge.ErrorPermissionDenied)
+				return
+			}
+			cb(centrifuge.PresenceStatsReply{}, nil)
+		})
+
+		client.OnDisconnect(func(e centrifuge.DisconnectEvent) {
+			log.Printf("user %s disconnected, disconnect: %s", client.UserID(), e.Disconnect)
+		})
+	})
+
+	// Single Tarantool.
+	mode := tntengine.ConnectionModeSingleInstance
+	shardAddresses := []string{"127.0.0.1:3301"}
+	if *ha {
+		// Single Tarantool RS with automatic leader election.
+		shardAddresses = []string{"127.0.0.1:3301,127.0.0.1:3302,127.0.0.1:3303"}
+		mode = tntengine.ConnectionModeLeaderFollower
+	} else if *sharded {
+		// Client-side sharding between two Tarantool instances.
+		shardAddresses = []string{"127.0.0.1:3301", "127.0.0.1:3302"}
+	}
+
+	var shards []*tntengine.Shard
+	for _, address := range shardAddresses {
+		shard, err := tntengine.NewShard(tntengine.ShardConfig{
+			Address:        address,
+			User:           "guest",
+			Password:       "",
+			ConnectionMode: mode,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		shards = append(shards, shard)
+	}
+
+	broker, err := tntengine.NewBroker(node, tntengine.BrokerConfig{
+		UsePolling: false,
+		Shards:     shards,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	presenceManager, err := tntengine.NewPresenceManager(node, tntengine.PresenceManagerConfig{
+		Shards: shards,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	node.SetBroker(broker)
+	node.SetPresenceManager(presenceManager)
+
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/connection/websocket", authMiddleware(centrifuge.NewWebsocketHandler(node, centrifuge.WebsocketConfig{})))
+	http.Handle("/", http.FileServer(http.Dir("./")))
+
+	go func() {
+		if err := http.ListenAndServe(":"+strconv.Itoa(*port), nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	waitExitSignal(node)
+	log.Println("bye!")
+}
---
+++ _examples/custom_engine_tarantool/readme.md
@@ -0,0 +1,103 @@
+This example shows how to use custom Engine implementation based on [Tarantool](https://www.tarantool.io/en/).
+
+This engine implementation provides much faster history and presence operations than Redis (up to 10x), while being on pair in subscribe and publish performance.
+
+**Important limitation to know**: Tarantool engine uses channels that start with `__` (two underscores) for internal needs and does not allow subscribing on them from the outside.
+
+Tarantool Engine PUB/SUB can be customized to work over PUSH or POLL.
+
+**Since this is just an example – we do not guarantee any stability here**. This engine implementation have not been tested in production environment.
+
+## Single Tarantool instance example
+
+Go to `tntserver` dir and install `indexpiration` rock dependency:
+
+```
+tarantoolctl rocks install https://raw.githubusercontent.com/moonlibs/indexpiration/master/rockspecs/indexpiration-scm-1.rockspec
+```
+
+Create a directory for Tarantool `snap` and `xlog` files:
+
+```
+mkdir tnt1
+```
+
+Start Tarantool:
+
+```
+tarantool init.lua 1
+```
+
+In another terminal start chat application running the following command from example directory:
+
+```
+go run main.go
+```
+
+Go to http://localhost:8000. You will see simple chat app, try writing a chat message in one browser tab – you should see it appears in another tab.
+
+## Client-side sharding
+
+Go to `tntserver` dir and create directories for Tarantool files:
+
+```
+mkdir tnt1
+mkdir tnt2
+```
+
+Start first Tarantool instance:
+
+```
+tarantool init.lua 1
+```
+
+Then second one:
+
+```
+tarantool init.lua 2
+```
+
+Now run first application instance on port 8000:
+
+```
+go run main.go --port 8000 --sharded
+```
+
+The second one on port 8001:
+
+```
+go run main.go --port 8001 --sharded
+```
+
+Go to http://localhost:8000 or http://localhost:8001 – nodes will be connected over Tarantool and data is consistently sharded between 2 different Tarantool instances (by a channel).
+
+## Tarantool high availability example
+
+**Requires Tarantool 2.6.1+ since example uses Raft-based replication**.
+
+Create directories for Tarantool files inside `tntserver` folder:
+
+```
+mkdir ha_tnt1 ha_tnt2 ha_tnt3
+```
+
+Run Tarantool cluster:
+
+```
+docker-compose up
+```
+
+Then start application:
+
+```
+go run main.go --port 8000 --ha
+```
+
+At this moment you can temporary stop/run one of Tarantool instances using:
+
+```
+docker-compose pause tnt1
+docker-compose unpause tnt1
+```
+
+See that chat application continues to work after a short downtime.
