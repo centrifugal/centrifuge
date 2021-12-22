34c5ea4583cc5d2176d11137fef2806de85f389a
Alexander Emelin <frvzmb@gmail.com>
2021-12-22 17:04:53 +0300

    support meta in redis engine

--- broker_redis.go
+++ broker_redis.go
@@ -351,6 +351,7 @@ func (b *RedisBroker) publish(s *RedisShard, ch string, data []byte, opts Publis
 	protoPub := &protocol.Publication{
 		Data: data,
 		Info: infoToProto(opts.ClientInfo),
+		Meta: opts.Meta,
 	}
 	byteMessage, err := protoPub.MarshalVT()
 	if err != nil {
