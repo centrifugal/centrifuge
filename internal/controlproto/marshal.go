5b3b1fad0d008c87023aa16534f5fd2552c1c010
Alexander Emelin <frvzmb@gmail.com>
2023-03-10 20:53:23 +0300

    One shot encode/decode for control proto (#263)

--- internal/controlproto/marshal.go
+++ internal/controlproto/marshal.go
@@ -5,14 +5,6 @@ import "github.com/centrifugal/centrifuge/internal/controlpb"
 // Encoder ...
 type Encoder interface {
 	EncodeCommand(*controlpb.Command) ([]byte, error)
-	EncodeNode(*controlpb.Node) ([]byte, error)
-	EncodeSubscribe(*controlpb.Subscribe) ([]byte, error)
-	EncodeUnsubscribe(*controlpb.Unsubscribe) ([]byte, error)
-	EncodeDisconnect(*controlpb.Disconnect) ([]byte, error)
-	EncodeSurveyRequest(request *controlpb.SurveyRequest) ([]byte, error)
-	EncodeSurveyResponse(request *controlpb.SurveyResponse) ([]byte, error)
-	EncodeNotification(request *controlpb.Notification) ([]byte, error)
-	EncodeRefresh(refresh *controlpb.Refresh) ([]byte, error)
 }
 
 var _ Encoder = (*ProtobufEncoder)(nil)
@@ -27,45 +19,5 @@ func NewProtobufEncoder() *ProtobufEncoder {
 
 // EncodeCommand ...
 func (e *ProtobufEncoder) EncodeCommand(cmd *controlpb.Command) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeNode ...
-func (e *ProtobufEncoder) EncodeNode(cmd *controlpb.Node) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeSubscribe ...
-func (e *ProtobufEncoder) EncodeSubscribe(cmd *controlpb.Subscribe) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeUnsubscribe ...
-func (e *ProtobufEncoder) EncodeUnsubscribe(cmd *controlpb.Unsubscribe) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeDisconnect ...
-func (e *ProtobufEncoder) EncodeDisconnect(cmd *controlpb.Disconnect) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeSurveyRequest ...
-func (e *ProtobufEncoder) EncodeSurveyRequest(cmd *controlpb.SurveyRequest) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeSurveyResponse ...
-func (e *ProtobufEncoder) EncodeSurveyResponse(cmd *controlpb.SurveyResponse) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeNotification ...
-func (e *ProtobufEncoder) EncodeNotification(cmd *controlpb.Notification) ([]byte, error) {
-	return cmd.MarshalVT()
-}
-
-// EncodeRefresh ...
-func (e *ProtobufEncoder) EncodeRefresh(cmd *controlpb.Refresh) ([]byte, error) {
 	return cmd.MarshalVT()
 }
