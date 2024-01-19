72f6efcee351e3d2d409637626e11427d7612d45
Alexander Emelin <frvzmb@gmail.com>
2024-01-19 21:15:31 +0200

    remove concurrent usage helper

--- internal/websocket/conn.go
+++ internal/websocket/conn.go
@@ -251,7 +251,6 @@ type Conn struct {
 	writeBufSize  int
 	writeDeadline time.Time
 	writer        io.WriteCloser // the current writer returned to the application
-	isWriting     bool           // for best-effort concurrent write detection
 
 	writeErrMu sync.Mutex
 	writeErr   error
@@ -609,22 +608,7 @@ func (w *messageWriter) flushFrame(final bool, extra []byte) error {
 		}
 	}
 
-	// Write the buffers to the connection with best-effort detection of
-	// concurrent writes. See the concurrency section in the package
-	// documentation for more info.
-
-	if c.isWriting {
-		panic("concurrent write to websocket connection")
-	}
-	c.isWriting = true
-
 	err := c.write(w.frameType, c.writeDeadline, c.writeBuf[framePos:w.pos], extra)
-
-	if !c.isWriting {
-		panic("concurrent write to websocket connection")
-	}
-	c.isWriting = false
-
 	if err != nil {
 		return w.endMessage(err)
 	}
@@ -741,16 +725,7 @@ func (c *Conn) WritePreparedMessage(pm *PreparedMessage) error {
 	if err != nil {
 		return err
 	}
-	if c.isWriting {
-		panic("concurrent write to websocket connection")
-	}
-	c.isWriting = true
-	err = c.write(frameType, c.writeDeadline, frameData, nil)
-	if !c.isWriting {
-		panic("concurrent write to websocket connection")
-	}
-	c.isWriting = false
-	return err
+	return c.write(frameType, c.writeDeadline, frameData, nil)
 }
 
 // WriteMessage is a helper method for getting a writer using NextWriter,
