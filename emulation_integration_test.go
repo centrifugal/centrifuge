96d34c2ad9d710971228378c8a3f1b07af38027e
FZambia <frvzmb@gmail.com>
2024-06-01 10:41:57 +0300

    test/bench dragonflydb

--- emulation_integration_test.go
+++ emulation_integration_test.go
@@ -35,7 +35,7 @@ func TestEmulation_DifferentNodes(t *testing.T) {
 		})
 	})
 
-	redisConf := testRedisConf()
+	redisConf := testSingleRedisConf()
 
 	s, err := NewRedisShard(n1, redisConf)
 	require.NoError(t, err)
